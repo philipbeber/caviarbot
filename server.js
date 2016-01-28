f886d08778ece9506951d0631de4e32829a744a5
Philip Beber <pbeber@fuzebox.com>
2016-01-28 14:28:54 -0800

    Added some cool stuff

--- server.js
+++ server.js
@@ -1,6 +1,50 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 1337;        // set our port
+
+var lastMessage;
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+router.route('/sms')
+    .post(function(req, res) {
+        console.log('Received sms: ' + JSON.stringify(req.body));
+        lastMessage = req.body;
+        res.send();
+    });
+
+router.route('/status')
+    .get(function(req, res) {
+        res.json({ lastMessage: lastMessage });
+    });
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/caviarbot', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
